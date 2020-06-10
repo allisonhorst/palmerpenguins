109faea551d40977cb107958256bf5a1f6be98a1
Allison Horst <ahorst@ucsb.edu>
2020-06-09 19:28:49 -0700

    update R documentation for penguins

--- R/penguins.R
+++ R/penguins.R
@@ -1,4 +1,4 @@
-#' Simplified dataset with observations for 3 penguin species (Adélie, Chinstrap and Gentoo) in Palmer Archipelago, Antarctica
+#' Size measurements for adult foraging penguins near Palmer Station, Antarctica
 #'
 #' Includes measurements for penguin species, island in Palmer Archipelago, size (flipper length, body mass, culmen dimensions), and sex.
 #'
@@ -12,7 +12,8 @@
 #'   \item{body_mass_g}{an integer denoting body mass (grams)}
 #'   \item{sex}{a character string denoting penguin sex}
 #' }
-#' @source \url{https://doi.org/10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f}
-#' @source \url{https://doi.org/10.6073/pasta/7fca67fb28d56ee2ffa3d9370ebda689}
-#' @source \url{https://doi.org/10.6073/pasta/c14dfcfada8ea13a17536e73eb6fbe9e}
+#' @source {Adélie penguins: Palmer Station Antarctica LTER and K. Gorman. 2020. Structural size measurements and isotopic signatures of foraging among adult male and female Adélie penguins (Pygoscelis adeliae) nesting along the Palmer Archipelago near Palmer Station, 2007-2009 ver 5. Environmental Data Initiative} \url{https://doi.org/10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f}
+#' @source {Gentoo penguins: Palmer Station Antarctica LTER and K. Gorman. 2020. Structural size measurements and isotopic signatures of foraging among adult male and female Gentoo penguin (Pygoscelis papua) nesting along the Palmer Archipelago near Palmer Station, 2007-2009 ver 5. Environmental Data Initiative} \url{https://doi.org/10.6073/pasta/7fca67fb28d56ee2ffa3d9370ebda689}
+#' @source {Chinstrap penguins: Palmer Station Antarctica LTER and K. Gorman. 2020. Structural size measurements and isotopic signatures of foraging among adult male and female Chinstrap penguin (Pygoscelis antarcticus) nesting along the Palmer Archipelago near Palmer Station, 2007-2009 ver 6. Environmental Data Initiative} \url{https://doi.org/10.6073/pasta/c14dfcfada8ea13a17536e73eb6fbe9e}
+#' @source {Originally published in: Gorman KB, Williams TD, Fraser WR (2014) Ecological Sexual Dimorphism and Environmental Variability within a Community of Antarctic Penguins (Genus Pygoscelis). PLoS ONE 9(3): e90081. doi:10.1371/journal.pone.0090081}
 "penguins"
