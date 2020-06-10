b51d1531f5af04e1ca8dce49187a5c2134d5060e
Allison Horst <ahorst@ucsb.edu>
2020-06-09 18:43:31 -0700

    update rda documentation w/ classes

--- R/penguins_raw.R
+++ R/penguins_raw.R
@@ -5,24 +5,24 @@
 #' @format A tibble with 344 rows and 17 variables:
 #' \describe{
 #'   \item{studyName}{Sampling expedition from which data were collected, generated, etc.}
-#'   \item{`Sample Number`}{Continuous numberng sequence for each sample}
-#'   \item{Species}{A string representing the species of an organism}
-#'   \item{Region}{Nominal region of Palmer LTER sampling grid}
-#'   \item{Island}{Island near Palmer Station where samples were collected}
-#'   \item{Stage}{Reproductive stage at sampling}
-#'   \item{`Individual ID`}{A unique ID for each individual in dataset}
-#'   \item{`Clutch Completion`}{Was the study nest observed with a full clutch, i.e., 2 eggs}
-#'   \item{`Date Egg`}{Date study nest observed with 1 egg (sampled)}
-#'   \item{`Culmen Length`}{Length of the dorsal ridge of a bird's bill (millimeters)}
-#'   \item{`Culmen Depth`}{Depth of the dorsal ridge of a bird's bill (millimeters)}
-#'   \item{`Flipper Length`}{Length of flipper (millimeters)}
-#'   \item{`Body Mass`}{Mass of body (grams)}
-#'   \item{Sex}{Code for the sex of an animal}
-#'   \item{`Delta 15 N`}{A measure of the ratio of stable isotopes 15N:14N}
-#'   \item{`Delta 13 C`}{A measure of the ratio of stable isotopes 13C:12C}
-#'   \item{Comments}{Text field to provide additional relevant information for data}
-
+#'   \item{`Sample Number`}{an integer denoting the continuous numbering sequence for each sample}
+#'   \item{Species}{a character string denoting the penguin species}
+#'   \item{Region}{a character string denoting the region of Palmer LTER sampling grid}
+#'   \item{Island}{a character string denoting the island near Palmer Station where samples were collected}
+#'   \item{Stage}{a character string denoting reproductive stage at sampling}
+#'   \item{Individual ID}{a character string denoting the unique ID for each individual in dataset}
+#'   \item{Clutch Completion}{a character string denoting if the study nest observed with a full clutch, i.e., 2 eggs}
+#'   \item{Date Egg}{a date denoting the date study nest observed with 1 egg (sampled)}
+#'   \item{Culmen Length}{a number denoting the length of the dorsal ridge of a bird's bill (millimeters)}
+#'   \item{Culmen Depth}{a number denoting the depth of the dorsal ridge of a bird's bill (millimeters)}
+#'   \item{Flipper Length}{an integer denoting the length penguin flipper (millimeters)}
+#'   \item{Body Mass}{an integer denoting the penguin body mass (grams)}
+#'   \item{Sex}{a character string denoting the sex of an animal}
+#'   \item{Delta 15 N}{a number denoting the measure of the ratio of stable isotopes 15N:14N}
+#'   \item{Delta 13 C}{a number denoting the measure of the ratio of stable isotopes 13C:12C}
+#'   \item{Comments}{a character string with text providing additional relevant information for data}
 #' }
+#'
 #' @source \url{https://doi.org/10.6073/pasta/98b16d7d563f265cb52372c8ca99e60f}
 #' @source \url{https://doi.org/10.6073/pasta/7fca67fb28d56ee2ffa3d9370ebda689}
 #' @source \url{https://doi.org/10.6073/pasta/c14dfcfada8ea13a17536e73eb6fbe9e}
