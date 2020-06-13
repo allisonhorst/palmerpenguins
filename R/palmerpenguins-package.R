c8dc057653f10225fa4953e317e164bdc2172dda
Alison Hill <apreshill@gmail.com>
2020-06-13 09:14:06 -0700

    redo namespace

--- R/palmerpenguins-package.R
+++ R/palmerpenguins-package.R
@@ -1,4 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @importFrom tibble tibble
 ## usethis namespace: end
 NULL
