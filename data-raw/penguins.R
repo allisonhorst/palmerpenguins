438ae7ec23915a13e568936b490a060feecb0be5
Allison Horst <ahorst@ucsb.edu>
2020-06-15 14:32:58 -0700

    update culmen to bill in penguins variable names

--- data-raw/penguins.R
+++ data-raw/penguins.R
@@ -8,13 +8,16 @@ penguins <- read_csv(here("data-raw","penguins_raw.csv"),
                      na = c("", "NA", ".")) %>%
   clean_names() %>%
   mutate(species_short = word(species, 1)) %>%
-  mutate(across(where(is.character), as.factor)) %>%
+  mutate(across(is.character, as.factor)) %>%
+  mutate(sex = as.factor(tolower(sex))) %>%
   mutate(flipper_length_mm = as.integer(flipper_length_mm)) %>%
   mutate(body_mass_g = as.integer(body_mass_g)) %>%
+  rename(bill_length_mm = culmen_length_mm,
+         bill_depth_mm = culmen_depth_mm) %>%
   select(species_short,
          island,
-         culmen_length_mm,
-         culmen_depth_mm,
+         bill_length_mm,
+         bill_depth_mm,
          flipper_length_mm,
          body_mass_g,
          sex) %>%
