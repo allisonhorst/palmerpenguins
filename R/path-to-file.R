4eeedbbf59c46023b0c8e3fd87f2198b00a1d186
Alison Hill <apreshill@gmail.com>
2020-07-01 13:50:45 -0700

    add path_to_file function to read in extdata csv files

---
+++ R/path-to-file.R
@@ -0,0 +1,24 @@
+#' Get file path to `penguins.csv` and `penguins_raw.csv` files
+#'
+#' palmerpenguins comes bundled with two csv files in its `inst/extdata`
+#' directory. This function make them easy to access.
+#'
+#' The data in each file is the same as \code{\link{penguins}} and \code{\link{penguins_raw}},
+#' but without the variable types.
+#'
+#' @param path Name of file in quotes with extension;
+#' `"penguins.csv"` and `"penguins-raw.csv"` will work.
+#' If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' path_to_file()
+#' path_to_file("penguins.csv")
+#' path_to_file("penguins_raw.csv")
+#' @source This function is adapted from \code{\link{readxl::readxl_example}}.
+path_to_file <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "palmerpenguins"))
+  } else {
+    system.file("extdata", path, package = "palmerpenguins", mustWork = TRUE)
+  }
+}
